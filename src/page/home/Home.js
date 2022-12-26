5495f6e83b871acb120874fca21c530769305316
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-12-26 23:39:12 +0530

    style added to home page

--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
+import './home.css';
+
 import Nav from '../../component/navbar/Nav';
 
 const Home = () => {
@@ -21,25 +23,33 @@ const Home = () => {
   // console.log(data.owner);
   return (
     <>
-      <div>
+      <div className='home-nav-cnt'>
         <Nav />
       </div>
-      {data?.map((item) => {
-        return (
-          <div key={item.id}>
-            <div>
-              <img className='picture' src={item.owner.picture} alt='poster' />
-              <p>
-                {item.owner.title} {item.owner.firstName} {item.owner.lastName}
-              </p>
+      <div className='home-main-cnt'>
+        {' '}
+        {data?.map((item) => {
+          return (
+            <div className='home-card-cnt' key={item.id}>
+              <div className='profile-data-cnt'>
+                <img
+                  className='picture'
+                  src={item.owner.picture}
+                  alt='poster'
+                />
+                <p className='profile-data-txt'>
+                  {item.owner.title} {item.owner.firstName}{' '}
+                  {item.owner.lastName}
+                </p>
+              </div>
+              <div className='posts-like'>
+                <img className='img' src={item.image} />
+                <p>{item.likes}</p>
+              </div>
             </div>
-            <div>
-              <img className='img' src={item.image} />
-              <p>{item.likes}</p>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </>
   );
 };
