8407f12ae97660070503f67359bf48e3822540da
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-12-26 19:35:07 +0530

    instead of fetch axios is used

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
-import axios from 'axios';
-import { useEffect } from 'react';
+
+import Home from './page/home/Home';
 
 function App() {
-  useEffect(() => {
-    fetch('https://dummyapi.io/data/v1/post?limit=10', {
-      headers: { 'APP-ID': '63a99f197fbab96d709a88e7' },
-    })
-      .then((resp) => resp.json())
-      .then((json) => console.log(json.data));
-  }, []);
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 const Home = () => {
+  useEffect(() => {
+    axios
+      .get('https://dummyapi.io/data/v1/post?limit=10', {
+        headers: { 'APP-ID': '63a9a922c7f9125ce4736f38' },
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  }, []);
+
   return <div></div>;
 };
 
