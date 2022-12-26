5ba8a19694493f1f2cadd3840f7292332ee60ba3
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-12-26 19:25:45 +0530

    home page created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    fetch('https://dummyapi.io/data/v1/post?limit=10', {
+      headers: { 'APP-ID': '63a99f197fbab96d709a88e7' },
+    })
+      .then((resp) => resp.json())
+      .then((json) => console.log(json.data));
+  }, []);
   return <div className='App'></div>;
 }
 
