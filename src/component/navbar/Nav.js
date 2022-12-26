5d5ffb6b3d77eb9af14436743c4c80f148663fdc
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-12-26 22:39:59 +0530

    nav bar, about page created

---
+++ src/component/navbar/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div>
+      <h2>blog !</h2>
+      <button> About Me</button>
+    </div>
+  );
+};
+
+export default Nav;
--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -1,18 +1,47 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+import Nav from '../../component/navbar/Nav';
 
 const Home = () => {
+  const [data, setData] = useState();
+
   useEffect(() => {
     axios
-      .get('https://dummyapi.io/data/v1/post?limit=10', {
+      .get('https://dummyapi.io/data/v1/post?limit=20', {
         headers: { 'APP-ID': '63a9a922c7f9125ce4736f38' },
       })
       .then((res) => {
         console.log(res.data);
+        console.log(res.data.data);
+
+        setData(res.data.data);
       });
   }, []);
-
-  return <div></div>;
+  // console.log(data.owner);
+  return (
+    <>
+      <div>
+        <Nav />
+      </div>
+      {data?.map((item) => {
+        return (
+          <div key={item.id}>
+            <div>
+              <img className='picture' src={item.owner.picture} alt='poster' />
+              <p>
+                {item.owner.title} {item.owner.firstName} {item.owner.lastName}
+              </p>
+            </div>
+            <div>
+              <img className='img' src={item.image} />
+              <p>{item.likes}</p>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Home;
